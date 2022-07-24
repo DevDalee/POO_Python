7a501070b8b74e2cb794d0444777d2d1d0de89f3
Marcos <mdclima12@gmail.com>
2022-07-24 10:14:02 -0300

    Atividade Completa

--- Lista02/Q013.py
+++ Lista02/Q013.py
@@ -1,45 +1,129 @@
-agenda = []
-def add_nome():
-    adnome = str(input("Digite o nome que deseja adicionar:"))
-    agenda[adnome].append("")
-def add_telefone():
-    nome = str(input("De quem é esse número?"))
-    if nome in agenda:
-        telefone = str(input("Digite o número desejado:"))
+from time import sleep
+
+agenda = {}
+telefones = []
+
+def menu():
+    print(f'-' * 30)
+    print(f'{"MENU DE OPÇÕES":^30}')
+    print(f'-' * 30)
+    print('[ 1 ] - Incluir novo nome')
+    print('[ 2 ] - Incluir novo telefone')
+    print('[ 3 ] - Excluir telefone')
+    print('[ 4 ] - Excluir nome')
+    print('[ 5 ] - Consultar telefone')
+    print('[-1 ] - Sair')
+    print(f'-' * 30)
+
+def incluir_nome(nome, telefones):
+    for chave, valor in agenda.items():
+        if (chave == nome):
+            for i in telefones:
+                valor.append(i)
+    print('ADICIONADO COM SUCESSO!!!')
+
+def incluir_telefone(nome, telefones):
+    for chave, valor in agenda.items():
+        if (chave == nome):
+            for i in telefones:
+                if (i not in valor):
+                    valor.append(i)
+    print('ADICIONADO COM SUCESSO!!!')  
+
+def excluir_telefone(nome, telefone):
+    for chave, valor in agenda.items():
+        if (chave == nome):
+            if (len(valor) > 1):
+                valor.remove(telefone)
+            elif(len(valor) == 1):
+                del agenda[nome]
+                break
+    print('EXCLUÍDO COM SUCESSO!!!')
+
+def excluir_pessoa(nome):
+    del agenda[nome]
+    print('EXCLUÍDO COM SUCESSO!!!')
+
+def consultar_telefone(nome):
+    if (nome not in agenda):
+        print('PESSOA NÃO CONSTA NA AGENDA!!!')
+        print('...')
+        sleep(1.5)
     else:
-        print("Nome não existente, deseja adicionar? (S/N)")
-        escolhaa = srt(input())
-        if escolhaa == S:
-            nomee = str(input("Digite o nome:"))
-            telefone = int(intput("Digite o número:"))
-            agenda[nomee].append(telefone)
-def del_telefone():
-    delet  = int(input("Digite o telefone que deseja deletar:"))
-    if delet in agenda:
-        agenda.pop(nome, delet)
-def del_nome():
-    aaa = str(input("Digite o nome da pessoa que deseja excluir da agenda"))
-    if aaa in agenda:
-        agenda.pop(aaa)
-def consultar():
-    numero = str(input("Quem deseja consultar os números?"))
-    if numero in agenda: 
-        print(agenda[nome])
-    else:
-        print("Essa pessoa não está na sua agenda!")
-a = 1
-while a > 0:
-    print("Digite o número do que deseja acessar: 1 - Incluir um novo nome, 2 - Incluir um telefone, 3 - Exclur um telefone, 4 - Excluir um nome ou -1 - Para sair!")
-    a = int(input())
-    if a == 1:
-        add_nome()
-    elif a == 2:
-        add_telefone()
-    elif a == 3:
-        del_telefone()
-    elif a == 4:
-        del_nome()
-    elif a == 5:
-        consultar()
-    elif a < 0:
+        print('NÚMEROS:')
+        for chave, valor in agenda.items():
+            if (chave == nome):
+                for i in valor:
+                    print(i)
+        print(f'-' * 30)
+        print('CONCLUÍDO!!!')
+
+while True:
+    telefones.clear()
+    menu()
+    op = int((input('Digite sua opção: ')))
+
+    if (op == 1):
+        nome = str(input('Digite o nome: ')).strip()
+        agenda[nome] = []
+        n_telef = int(input('Digite quantos n° de telefone quer adiconar: '))
+
+        for i in range(n_telef):
+            telefone = str(input('Digite o n° de telefone: ')).strip()
+            if (telefone not in telefones):
+                telefones.append(telefone)
+        
+        print(f'-' * 30)
+        print('Adicionando...')
+        sleep(1.5)
+        print(f'-' * 30)
+        incluir_nome(nome, telefones)
+
+    elif (op == 2):
+        telefone = str(input('Digite o n° de telefone: ')).strip()
+        telefones.append(telefone)
+        nome = str(input('Digite o nome que deseja adicionar o novo número: ')).strip()
+
+        if (nome not in agenda.keys()):
+            print('Nome não existe na agenda, adicionando...')
+            print(f'-' * 30)
+            sleep(1.5)
+            agenda[nome] = []
+            incluir_nome(nome, telefones)
+        else:
+            print(f'-' * 30)
+            print('Adicionando...')
+            sleep(1.5)
+            print(f'-' * 30)
+            incluir_telefone(nome, telefones)
+
+    elif (op == 3):
+        telefone = str(input('Digite o n° de telefone a ser excluído: ')).strip()
+        nome = str(input('Nome responsável pelo telefone: ')).strip()
+        
+        print(f'-' * 30)
+        print('Excluindo...')
+        sleep(1.5)
+        print(f'-' * 30)
+        excluir_telefone(nome, telefone)
+
+    elif (op == 4):
+        nome = str(input('Nome a ser excluído da agenda: ')).strip()
+
+        print(f'-' * 30)
+        print('Excluindo...')
+        sleep(1.5)
+        print(f'-' * 30)
+        excluir_pessoa(nome)
+
+    elif (op == 5):
+        nome = str(input('Nome de quem deseja fazer a consulta na agenda: ')).strip()
+
+        print(f'-' * 30)
+        print('Pesquisando...')
+        sleep(1.5)
+        print(f'-' * 30)
+        consultar_telefone(nome)
+
+    elif (op == -1):
         break
