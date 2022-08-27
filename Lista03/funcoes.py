fe6df2f0e094c1bf44702d52c61676b574a341b6
Marcos Dalessandro <devdalee@Marcoss-MacBook-Pro.local>
2022-08-27 11:07:14 -0300

    Lista 03 Com mudanças completa

--- Lista03/funcoes.py
+++ Lista03/funcoes.py
@@ -1,58 +1,124 @@
-from random import randint
+from conta import Pessoa
+from titular import Conta
 
-class Conta:
+def menu():
+    print('-' * 30)
+    print('Contas disponíveis: \n')
+    c.listar_contas(contas)
+    print('-' * 30)
 
-    def __init__(self, titular, numero, saldo=0):
-        self.titular = titular
-        self.numero = numero
-        self.saldo = saldo
+contas = {}
 
-    def criar_conta(self, numero):
-        numero = randint(100, 1000)
-        self.numero = str(numero)
+while True:
+    print(f'{"MENU PRINCIPAL":^30}')
+    print('-' * 30)
+    print('[1] - Criar conta')
+    print('[2] - Listar contas')
+    print('[3] - Sacar valor')
+    print('[4] - Depositar valor')
+    print('[5] - Realizar transferência')
+    print('[6] - Consultar Histórico')
+    print('[7] - Excluir conta')
+    print('[0] - Sair')
+    print('-' * 30)
+    resp = int(input('Digite sua opção: '))
 
+    if (resp == 0):
+        break
+    elif (resp == 1):
+        nome = str(input('Digite o nome do titular da conta: ')).strip()
+        cpf = str(input('Digite o CPF: ')).strip()
+        tit = Conta(nome, cpf)
+        c = Pessoa(tit, '', 0.0)
+
+        numero = c.criar_conta('')
+        if (numero not in contas):    
+            contas[numero] = c
+        else:
+            while (numero in contas):
+                numero = c.criar_conta('')
+            contas[numero] = c
+
+    elif (resp == 2):
         print('-' * 30)
-        print('Conta criada com sucesso!')
-        print(f'{self.numero} é o número da conta!')
+        print()
+
+        if (len(contas) == 0):
+            print('Nenhuma conta existente!')
+        else:
+            c.listar_contas(contas)
         print('-' * 30)
-        return str(numero)
 
-    def sacar(self, valor):
-        if valor <= self.saldo and valor > 0:
-            self.saldo -= valor
-            return f'Saque Realizado Com Sucesso'
+    elif (resp == 3):
+        menu()
+        numsc = str(input('Número da conta para saque: ')).strip()
+        
+        if numsc in contas:
+            valor = float(input('Digite o valor: R$'))
+            print('-' * 30)
+
+            if(contas[numsc].sacar(valor)):
+                print('Saque efetuado com sucesso!')
+            else:
+                print('Saldo insuficiente.')
         else:
-            return f'Operação não realizada'
+            print('Conta inválida!')
+        print('-' * 30)
 
-    def extrato(self):
-        print(f'Titular da conta: {self.titular.nome}')
-        print(f'CPF do titular: {self.titular.cpf}')
-        print(f'Número da conta: {self.numero}')
-        print(f'Saldo da conta: R${self.saldo:.2f}')
-        print('\n')   
+    elif (resp == 4):
+        menu()
+        numdp = str(input('Número da conta para depósito: ')).strip()
 
-    def depositar(self, valor):
-        if valor > 0:
-            self.saldo += valor
-            return f'Deposito Efetuado Com Sucesso'
+        if numdp in contas:
+            valor = float(input('Digite o valor: R$'))
+            print('-' * 30)
+
+            if(contas[numdp].depositar(valor)):
+                print('Depósito efetuado com sucesso!')
+            else:
+                print('Valor inválido!')
         else:
-            return f'Operação não realizada'  
+            print('Conta inválida!')
+        print('-' * 30)
 
-    def excluir(self, numero):
-        if numero in Conta:
-            Conta.pop(numero)
-            return f'Operação Realizada Com Sucesso!'
+    elif (resp == 5):
+       menu()
 
-    def listaContas(self, contas):
-        for chave, valor in contas.items():
-            valor.extrato()
+       c_origem = str(input('Número da conta de origem: ')).strip()
+       c_destino = str(input('Número da conta de destino: ')).strip()
 
-    def transferir(self, destino, valor):
-        operacao = self.sacar(valor)
+       if (c_origem in contas and c_destino in contas):
+            val_transf = float(input('Valor a ser transferido: R$'))
+            print('-' * 30)
 
-        if operacao == False:
-            return False
+            if(contas[c_origem].transfere_para(contas[c_destino], val_transf)):
+                print('Transferência realizada com sucesso!')
+            else:
+                print('ERRO! Saldo insuficiente na conta de origem.')
+       else:
+            print('Conta inválida!')
+       print('-' * 30)
+
+    elif (resp == 6):
+        menu()
+        n_conta = str(input('Número da conta para consulta de histórico: ')).strip()
+        print('-' * 30)
+
+        if (n_conta in contas):
+            contas[n_conta].historico.imprime_t()
         else:
-            destino.depositar(valor)
-            return True
-    
+            print('Conta inválida!')
+        print('-' * 30)
+
+    elif (resp == 7):
+       menu()
+
+       numex = str(input('Número da conta a ser excluída:'))
+       print('-' * 30)
+
+       if contas[numex].saldoc > 0:
+            print('Saque o valor presente na conta para excluir!')
+       else:
+            contas.pop(numex)
+            print('Conta excluída com sucesso!')
+       print('-' * 30)
--- teste.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-s = list(range(1,100))
-s.sort()
-print(len(s))
-print(np.median(s))
-print(np.var(s))
-print(np.std(s))
