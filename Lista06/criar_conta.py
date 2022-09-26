b343058791e21840133ebf08e20d202f2e8eea1c
DevDalee <mdclima12@gmail.com>
2022-09-25 21:03:23 -0300

    Lista Completa

--- Lista06/criar_conta.py
+++ Lista06/criar_conta.py
@@ -1,31 +1,119 @@
+from abc import ABC
 from random import randint
-from functions import Historico
-class Funcionario():
-    def __init__(self, nome, cpf, dataNasc, salario = 1200):
-        self._nome = nome
-        self._cpf = cpf
-        self._dataNasc = dataNasc
-class Cliente():
-    def __init__(self, nome, cpf, dataNasc, profissao):
-        self._nome = nome
-        self._cpf = cpf
-        self._dataNasc = dataNasc
-        self._profissao = profissao
-        
-    def cria_conta_corrente(self, numero, historico, saldo = 0 ):
-        numero = randint(1000, 10000)
-        self.numeroc = numero
-        
-        print('-'*20)
-        print('Conta Criada Com Sucesso!')
-        print(f'{self.numeroc} é o número da conta!')     
-        return str(numero)
-        
-    def cria_conta_poup(self, numero, historico ,saldo = 0):
-        numero = randint(100,1000)
-        self.numerop = numero
-        print('-'*20)
-        print('Conta Criada Com Sucesso!')
-        print(f'{self.numerop} é o número da conta!')
-        print('-'*20)
-        return str(numero)
+from criar_conta import *
+import datetime
+
+bd_Contas = {}
+
+class Historico:
+
+    def __init__(self):
+        self.__data_abertura = datetime.datetime.today()
+        self.__transacoes = []
+    
+    @property
+    def data_abertura(self):
+        return self.__data_abertura
+    
+    @property
+    def transacoes_c(self):
+        return self.__transacoes
+
+    def imprime_t(self):
+        print(f'Data de abertura: {self.data_abertura}')
+        print('Transações: ')
+        for t in self.transacoes_c:
+            print('-', t)
+
+class Conta(ABC):
+    def __init__(self, numero = 0, saldo = 0):
+        self.__numero = numero
+        self.__saldo = saldo
+        self.__historico = Historico()
+    
+    @property
+    def get_numero(self):
+        return self.__numero
+
+    @get_numero.setter
+    def set_numero(self, numero):
+        self.__numero = numero
+
+    @property
+    def get_saldo(self):
+        return self.__saldo
+    
+    @get_saldo.setter
+    def set_saldo(self, saldo):
+        if saldo > 0:
+            self.__saldo = saldo
+            return True
+        else:
+            return False
+
+    @property
+    def get_historico(self):
+        return self.__historico
+
+    def gera_numero(self):
+        numero = randint(100, 999)
+        self.set_numero = numero
+    
+
+    def cria_conta(self, cpf, conta):
+        if (cpf not in bd_Contas):
+            bd_Contas[cpf] = []
+            bd_Contas[cpf].append(conta)
+        elif (len(bd_Contas[cpf]) == 1):
+            bd_Contas[cpf].append(conta)
+        else:
+            return False
+
+        return True
+    
+    def sacar(self, valor):
+        if self.get_saldo > 0:
+            self.set_saldo = self.get_saldo - valor
+            self.get_historico.transacoes_c.append(f'Saque de R${valor} - Data: {datetime.datetime.today()}')
+            return True
+        else:
+            return False
+    
+    def depositar(self, valor):
+        if (valor > 0):
+            self.set_saldo = self.get_saldo + valor
+            self.get_historico.transacoes_c.append(f'Depósito de R${valor} - Data: {datetime.datetime.today()}')
+            return True
+        else:
+            return False
+    
+    def transfere(self, cpf_dest, tipo_destino, valor):
+        retirou = self.sacar(valor)
+
+        if (retirou):
+            for classe in bd_Contas[cpf_dest]:
+                if classe.get_tipo == tipo_destino:
+                    classe.depositar(valor)
+            
+            self.get_historico.transacoes_c.append(f'Transferência de R${valor} - Para conta de {bd_Contas[cpf_dest].get_numero}')
+            return True
+        else:
+            return False
+
+class ContaCorrente(Conta):
+    def __init__(self, numero = 0, saldo = 0, tipo = 'Conta Corrente'):
+        super().__init__(numero, saldo)
+        self.__tipo = tipo
+    
+    @property
+    def get_tipo(self):
+        return self.__tipo
+
+class ContaPoupanca(Conta):
+    def __init__(self, numero = 0, saldo = 0, tipo = 'Conta Poupança'):
+        super().__init__(numero, saldo)
+        self.__tipo = tipo
+    
+    @property
+    def get_tipo(self):
+        return self.__tipo
--- Lista06/functions.py
+++ Lista06/functions.py
@@ -1,75 +1,147 @@
-from criar_conta import * 
-from datetime import datetime
-from abc import *
+from xml.dom import VALIDATION_ERR
+from clientes import *
+from funcionarios import *
+from criar_conta import *
 
-class calc_tributacao(ABC):
-    @abstractmethod
-    def calc_tributacao(self, saldo_cc, valor_sv):
-        pass
-class Historico:
-
+class Tributacoes:
     def __init__(self):
-        self.__data_abertura = datetime.today()
-        self.__transacoes = []
-    
-    @property
-    def data_abertura(self):
-        return self.__data_abertura
-    
-    @property
-    def transacoes_c(self):
-        return self.__transacoes
+        self.__tributacoes_hist = []
 
-    def imprime_t(self):
-        print(f'Data de abertura: {self.data_abertura}')
-        print('Transações: ')
-        for t in self.transacoes_c:
-            print('-', t)
-
-class Funcs():
-    def __init__(self, numero, saldo):
-        self.numero = numero
-        self.historico = Historico() 
-        self.saldo = saldo
-        
     @property
-    def saldoc(self):
-        return self.__saldo
-    
-    @saldoc.setter
-    def saldoc(self, valor):
-        self.__saldo = valor
-        
-    @property
-    def historico(self):
-        return self.__historico
-    
-    def depositar(self, valor):
-        atual = 0
-        if (valor > 0):
-            atual = self.saldoc
-            atual += valor
-            self.saldoc = atual
-            return True
-        else:
-            return False
-        
-    def sacar(self, valor):
-        atual = 0
-        if (valor <= self.saldoc and valor > 0):
-            atual = self.saldoc
-            atual -= valor
-            self.saldoc = atual
-            return True
-        else:
-            return False
-        
-    def transfere_para(self, destino, valor):
-        retirou = self.sacar(valor)
+    def get_tributacoes_hist(self):
+        return self.__tributacoes_hist
 
-        if (retirou == False):
+class Functions:
+
+    def __init__(self, num_contas = 0):
+        self.__tributacao = Tributacoes()
+        self.__num_contas = num_contas
+    
+    @property
+    def get_tributacao(self):
+        return self.__tributacao
+    
+    @property
+    def get_num_contas(self):
+        return self.__num_contas
+    
+    @get_num_contas.setter
+    def set_num_contas(self, num):
+        self.__num_contas += num
+    
+    def mostrar_contas(self):
+        if (len(bd_Contas) == 0):
             return False
         else:
-            destino.depositar(valor)
-            self.historico.transacoes_c.append(f'Transferência de R${valor} - Para conta {destino.numeroc}')
+            print(f'{"CPFs cadastrados":^40}')
+            for chave, valor in bd_Contas.items():
+                print(f'CPF -> {chave}')
+                
+                for i in valor:
+                    print(f'{i.get_tipo}')
+                print()
+    
+    def mostrar_clientes(self):
+        if len(bd_Contas) > 0:
+            for cpf_cont in bd_Contas.keys():
+                for cpf_cli in bd_Clientes.keys():
+                    if cpf_cont == cpf_cli:
+                        print(f'Nome -> {bd_Clientes[cpf_cont].get_nome}')
+                        print(f'Número de contas -> {bd_Clientes[cpf_cont].get_qnt_contas}')
+                if (cpf_cont in seguros):
+                    print(f'Seguros de vida -> {len(seguros[cpf_cont])}')
+                else:
+                    print('Seguros de vida -> 0')
+
+                print()
             return True
+        else:
+            return False        
+
+
+    def verifica_cadadastro(self, cpf_cadastrado):
+        bd_Clientes[cpf_cadastrado].verifica_cadastro
+
+    def criar_conta_corrente(self, cpf, tipo = 'Conta Corrente'):
+        conta_corrente = ContaCorrente()
+        try:
+            conta_corrente.cria_conta(cpf, conta_corrente)
+            if(bd_Clientes[cpf].num_contas(cpf, tipo)):
+                self.set_num_contas = 1
+                return True
+        except:
+            return False
+    
+    def criar_conta_poupanca(self, cpf, tipo = 'Conta Poupança'):
+        conta_poupanca = ContaPoupanca()
+        
+        try:
+            conta_poupanca.cria_conta(cpf, conta_poupanca)
+            if(bd_Clientes[cpf].num_contas(cpf, tipo)):
+                self.set_num_contas = 1
+                return True
+        except:
+            return False
+    
+    def criar_seguro(self, cpf, valor_mens, valor_tot):
+        seguro = SeguroDeVida(cpf, valor_mens, valor_tot)
+        seguro.criar_seguro(cpf, seguro)
+    
+    def operacao(self, cpf, tipo_op, valor, tipo_conta):
+        if (tipo_op == 7):
+            
+            for classe in bd_Contas[cpf]:
+                if classe.get_tipo == tipo_conta:
+                    classe.sacar(valor)
+            return True
+
+        elif (tipo_op == 8):
+
+            for classe in bd_Contas[cpf]:
+                if classe.get_tipo == tipo_conta:
+                    classe.depositar(valor)
+            return True
+
+        else:
+            return False
+    
+    def transfere(self, cpf_origem, cpf_destino, valor, tipo_conta_origem, tipo_conta_destino):
+        try:
+            for classe in bd_Contas[cpf_origem]:
+                if classe.get_tipo == tipo_conta_origem:
+                    classe.transfere(cpf_destino, tipo_conta_destino, valor)
+                return True
+        except:
+            return False
+
+    def historico(self, cpf, tipo_conta):
+        try:            
+            for classe in bd_Contas[cpf]:
+                if classe.get_tipo == tipo_conta:
+                    classe.get_historico.imprime_t()
+            return True
+        except:
+            return False
+    
+    def tributacao(self):
+        tributacao_clientes = {}
+        soma_trib = 0
+
+        for cpf, valor in bd_Contas.items():
+            for conta in valor:
+                if conta.get_tipo == 'Conta Corrente':
+
+                    if cpf in seguros:
+                        for classe in seguros[cpf]:
+                            tributacao_clientes[cpf] = 10 + 0.01 * conta.get_saldo + 0.02 * classe.get_valor_mensal
+                    else:
+                        return False
+
+        print(tributacao_clientes)
+        for valor in tributacao_clientes.values():
+            soma_trib += valor
+
+        self.get_tributacao.get_tributacoes_hist.append(soma_trib)
+        return self.get_tributacao.get_tributacoes_hist
+    
+    
